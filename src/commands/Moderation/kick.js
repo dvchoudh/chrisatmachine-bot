da2823bfa624b7a3cab167709bb8bb2897a50dfb
KittyBorgX <krishnaramasimha12@gmail.com>
2021-08-07 14:22:00 +0530

    add slowmode and kick commands

---
+++ src/commands/Moderation/kick.js
@@ -0,0 +1,54 @@
+//@ts-check
+
+const { setCooldown } = require("../../utils/utils");
+const { MessageEmbed } = require("discord.js");
+/**
+ * @type {import('../../typings.d').Command}
+ */
+module.exports = {
+  name: "kick",
+  category: "Moderation",
+  aliases: ["kickmembers"],
+  clientPerms: ["KICK_MEMBERS"],
+
+  execute: async function ({ client, message, args }) {
+    setCooldown(client, this, message);
+    let member = message.mentions.members.first();
+    if (!member)
+      return message.reply("Please specify a member for me to kick them");
+    let reason = args.slice(1).join(" ");
+    if (!reason) reason = "No Reason Given";
+    if (!member.kickable) return message.reply("This member is not kickable");
+    try {
+      const sembed2 = new MessageEmbed()
+        .setColor("RED")
+        .setDescription(
+          `**You Have Been Kicked From ${message.guild.name} for - ${
+            reason || "No Reason!"
+          }**`
+        )
+        .setFooter(message.guild.name, message.guild.iconURL());
+      member
+        .send(sembed2)
+        .then(() => member.kick())
+        .catch(() => null);
+    } catch {
+      member.kick();
+    }
+
+    if (reason) {
+      var sembed = new MessageEmbed()
+        .setColor("GREEN")
+        .setDescription(
+          `**${member.user.username}** has been kicked for ${reason}`
+        );
+      message.channel.send(sembed);
+    } else {
+      var sembed2 = new MessageEmbed()
+        .setColor("GREEN")
+        .setDescription(`**${member.user.username}** has been kicked`);
+      message.channel.send(sembed2);
+    }
+    // member.kick(reason).catch((err) => console.log(err));
+  },
+};
---
+++ src/commands/Moderation/slowmode.js
@@ -0,0 +1,37 @@
+//@ts-check
+
+const { MessageEmbed } = require("discord.js");
+const ytsr = require("ytsr");
+const { red } = require("../../../config/colors.json");
+
+//@ts-check
+
+const { setCooldown } = require("../../utils/utils");
+
+/**
+ * @type {import('../../typings.d').Command}
+ */
+module.exports = {
+  name: "slowmode",
+  category: "Moderation",
+  aliases: ["sm"],
+  clientPerms: ["MANAGE_MESSAGES"],
+
+  execute: async function ({ client, message, args }) {
+    // @ts-ignore
+    if (!args[0]) return message.reply("You must specify a time in seconds.");
+    const time = parseInt(args[0]);
+    if (isNaN(time))
+      return message.reply("You must specify a number of seconds.");
+    if (time < 0)
+      return message.reply(
+        "You must specify a number of seconds greater than 0."
+      );
+    if (time > 21600)
+      return message.reply("You cannot set a slowmode longer than 6 hours.");
+
+    // @ts-ignore
+    message.channel.setRateLimitPerUser(args[0]);
+    message.channel.send(`Slowmode has been set to: ${args[0]} Seconds`);
+  },
+};
