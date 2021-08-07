dd8c9e2d367ff33b8fa9a43b7cc85fd4eebd48a8
KittyBorgX <krishnaramasimha12@gmail.com>
2021-08-07 18:53:22 +0530

    Update configs to .env

--- src/commands/Misc/say.js
+++ src/commands/Misc/say.js
@@ -1,18 +1,19 @@
+//@ts-check
+
 const { setCooldown } = require("../../utils/utils");
 
-const TicTacToe = require("discord-tictactoe");
-const game = new TicTacToe({ language: "en" });
 /**
  * @type {import('../../typings.d').Command}
  */
 module.exports = {
-  name: "ttt",
-  category: "Fun",
-  aliases: ["tictactoe-ai", "tictactoe"],
+  name: "say",
+  category: "Misc",
+  aliases: ["saythis"],
   clientPerms: ["SEND_MESSAGES"],
 
   execute: async function ({ client, message, args }) {
     setCooldown(client, this, message);
-    game.handleMessage(message);
+    const msg = args.join(" ");
+    message.channel.send(msg);
   },
 };
