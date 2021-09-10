039c618afceb3c1239406e3a0c61a088c913b042
tamton-aquib <aquibjavedt007@gmail.com>
2021-09-10 13:02:01 +0530

    adding stuff to docs.js

--- src/commands/Lunarvim-Docs/docs.js
+++ src/commands/Lunarvim-Docs/docs.js
@@ -1,65 +1,90 @@
 const { Permissions, Client, Message, MessageEmbed } = require("discord.js");
 
+/**
+*
+* @param {Client} client
+* @param {Message} message
+* @param {STring[]} args
+*/
 module.exports = {
-  name: "docs",
-  description: "Lunarvim docs!",
-  perms: {
-    client: [
-      Permissions.FLAGS.SEND_MESSAGES,
-      Permissions.FLAGS.VIEW_CHANNEL,
-      Permissions.FLAGS.EMBED_LINKS,
-    ],
-    user: [Permissions.FLAGS.SEND_MESSAGES],
-  },
-  aliases: [],
+	name: "docs",
+	description: "Lunarvim docs!",
+	perms: {
+		client: [
+			Permissions.FLAGS.SEND_MESSAGES,
+			Permissions.FLAGS.VIEW_CHANNEL,
+			Permissions.FLAGS.EMBED_LINKS,
+		],
+		user: [Permissions.FLAGS.SEND_MESSAGES],
+	},
+	aliases: ["docs", "doc", "documentation", "rtfm"],
 
-  /**
+	/**
    *
    * @param {Client} client
    * @param {Message} message
-   * @param {STring[]} args
-   */
-  execute: async (client, message, args) => {
-    const search_query = args[0];
-    const args2 = args[1];
+   * @param {String[]} args
+	*/
+	execute: async (client, message, args) => {
+		const search_query = args.join("-");
+		// const args2 = args[1];
 
-    if (!search_query) {
-      const embed = new MessageEmbed()
-        .setTitle("Lunarvim Docs")
-        .setDescription(
-          "Search For Documentation about LunarVim \n\n Usage: `.docs searchQuery` \n\n Example: `.docs install`"
-        )
-        .setColor("RED");
-      message.channel.send({ embeds: [embed] });
-    }
-    // game.handleMessage(message);
-    const lunarDocs = {
-      installing: "https://www.lunarvim.org/01-installing.html",
-      stable: "https://www.lunarvim.org/01-installing.html#stable",
-      rolling: "https://www.lunarvim.org/01-installing.html#rolling",
+		// TODO: need to add categories: languages, extra-plugins
+		const lunarDocs = {
+			"lunarvim-github": "https://github.com/LunarVim/LunarVim",
+			"installing-uninstalling": "https://www.lunarvim.org/01-installing.html",
+			"stable": "https://www.lunarvim.org/01-installing.html#stable",
+			"rolling": "https://www.lunarvim.org/01-installing.html#rolling",
 
-      quickstart: "https://www.lunarvim.org/02-after-install.html",
-      "general-settings":
-        "https://www.lunarvim.org/configuration/01-settings.html",
-      "keybindings-keybinds-keymaps-keymappings":
-        "https://www.lunarvim.org/configuration/02-keybindings.html",
-      "which-key-whichkey":
-        "https://www.lunarvim.org/configuration/02-keybindings.html#whichkey-bindings",
-      "color-schemes-colorschemes":
-        "https://www.lunarvim.org/configuration/03-colorschemes.html",
-      "nerd-fonts": "https://www.lunarvim.org/configuration/04-nerd-fonts.html",
-      "autocmds-autocommands":
-        "https://www.lunarvim.org/configuration/05-autocommands.html",
-    };
-    Object.keys(lunarDocs).map((query) => {
-      if (query.includes(search_query)) {
-        const em = new MessageEmbed()
-          .setTitle("LunarVim docs search results")
-          .setDescription(lunarDocs[query])
-          .setColor("RED");
+			"configuration": "https://www.lunarvim.org/configuration/",
+			"nerd-fonts": "https://www.lunarvim.org/configuration/04-nerd-fonts.html",
+			"which-key-whichkey": "https://www.lunarvim.org/configuration/02-keybindings.html#whichkey-bindings",
 
-        message.channel.send({ embeds: [em] });
-      }
-    });
-  },
+			"quickstart-after-install": "https://www.lunarvim.org/02-after-install.html",
+			"general-settings": "https://www.lunarvim.org/configuration/01-settings.html",
+			"keybindings-keybinds-keymaps-keymappings": "https://www.lunarvim.org/configuration/02-keybindings.html",
+			"autocmds-autocommands": "https://www.lunarvim.org/configuration/05-autocommands.html",
+			"color-schemes-colorschemes": "https://www.lunarvim.org/configuration/03-colorschemes.html",
+			"statusline-lualine-galaxyline": "https://www.lunarvim.org/configuration/06-statusline.html",
+
+			"core-plugins-dashboard-bufferline-terminal-toggleterm": "https://www.lunarvim.org/plugins/#core-plugins",
+			"install-plugins": "https://www.lunarvim.org/plugins/01-installing.html#example",
+			"remove-plugins-delete-plugins": "https://www.lunarvim.org/plugins/01-installing.html#removing-plugins",
+			"style-guide": "https://www.lunarvim.org/dev/#style-guide",
+
+			"langs-cpp-javascript-php-python-rust-ruby-lua": "https://www.lunarvim.org/languages/",
+			"lvinfo-lunarvim-info": "https://www.lunarvim.org/languages/#lunarvim-info",
+			"install-lsp-lspinstall": "https://www.lunarvim.org/languages/#lsp",
+			"formatter-formatting": "https://www.lunarvim.org/languages/#formatting",
+			"linting": "https://www.lunarvim.org/languages/#linting",
+
+			// PLUGINS
+			"vim-fugitive": "https://www.lunarvim.org/plugins/02-extra-plugins.html#vim-fugitive",
+			"trouble": "https://www.lunarvim.org/plugins/02-extra-plugins.html#trouble-nvim",
+			"indent-blanklines": "https://www.lunarvim.org/plugins/02-extra-plugins.html#indent-blankline",
+			"compe-cmp-tabnine": "https://www.lunarvim.org/plugins/02-extra-plugins.html#compe-tabnine",
+			"telescope": "https://www.lunarvim.org/plugins/02-extra-plugins.html#telescope-fzy-native-nvim",
+
+			"nice-noice-bruh": "[LMAO loser](https://www.youtube.com/watch?v=dQw4w9WgXcQ)"
+		};
+
+		let count = 0;
+		Object.keys(lunarDocs).map((query) => {
+			if (query.includes(search_query)) {
+				count++;
+				const em = new MessageEmbed()
+					// .setTitle("LunarVim docs search results")
+					.setDescription(lunarDocs[query])
+					.setColor("RED");
+
+				message.channel.send({ embeds: [em] });
+			}
+		});
+		if (count <=0) {
+			const failed = new MessageEmbed()
+				.setDescription(`Couldn't get docs for ${search_query}`)
+				.setColor("RED");
+			message.channel.send({embeds: [failed]})
+		}
+	},
 };
