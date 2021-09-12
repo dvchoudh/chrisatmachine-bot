80f6b89119a37c0818b4c951136905febbd68062
tamton-aquib <aquibjavedt007@gmail.com>
2021-09-12 10:31:00 +0530

    fix: reaction author instead of message author

--- src/events/messageReactionAdd.js
+++ src/events/messageReactionAdd.js
@@ -6,17 +6,16 @@ const { Client, MessageEmbed } = require("discord.js");
  * @param {NoIdea} payload
  */
 
-module.exports = async (client, payload) => {
+module.exports = async (client, payload, user) => {
 	const content = await payload.message.content
 	const emoji = await payload.emoji.name
-	const author = await payload.message.author
 
 	if (content) {
 		if ("🔖📑".includes(emoji)) {
 			const em = new MessageEmbed()
 				.setDescription(content)
 				.setColor("RED")
-			await author.send({embeds: [em]})
+			await user.send({embeds: [em]})
 		}
 	}
 };
