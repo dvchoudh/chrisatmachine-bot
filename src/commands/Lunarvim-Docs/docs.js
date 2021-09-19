d225c225bfbfbc72e54bf1dc4be1db3d4b82d0f0
tamton-aquib <aquibjavedt007@gmail.com>
2021-09-19 09:34:12 +0530

    moving docs object above the command function so it doesnt get created on every calls, and improved docs

--- src/commands/Lunarvim-Docs/docs.js
+++ src/commands/Lunarvim-Docs/docs.js
@@ -1,5 +1,63 @@
 const { Permissions, Client, Message, MessageEmbed } = require("discord.js");
 
+// TODO: need to add categories: languages, extra-plugins
+const lunarDocs = {
+	"lunarvim-github": "https://github.com/LunarVim/LunarVim",
+	"installing-uninstalling": "https://www.lunarvim.org/01-installing.html#uninstall",
+	"stable": "https://www.lunarvim.org/01-installing.html#stable",
+	"rolling": "https://www.lunarvim.org/01-installing.html#rolling",
+
+	"configuration": "https://www.lunarvim.org/configuration/",
+	"nerd-fonts": "https://www.lunarvim.org/configuration/04-nerd-fonts.html",
+	"which-key-whichkey": "https://www.lunarvim.org/configuration/02-keybindings.html#whichkey-bindings",
+
+	"quickstart-after-install": "https://www.lunarvim.org/02-after-install.html",
+	"general-settings-options": "https://www.lunarvim.org/configuration/01-settings.html",
+	"keybindings-keybinds-keymaps-keymappings": "https://www.lunarvim.org/configuration/02-keybindings.html",
+	"autocmds-autocommands": "https://www.lunarvim.org/configuration/05-autocommands.html",
+	"color-schemes-colorschemes": "https://www.lunarvim.org/configuration/03-colorschemes.html",
+	"statusline-lualine-galaxyline": "https://www.lunarvim.org/configuration/06-statusline.html",
+
+	"general-keybinds-general-keybindings": "https://www.lunarvim.org/configuration/02-keybindings.html#general-bindings",
+	"leader-key": "https://www.lunarvim.org/configuration/02-keybindings.html#leader-key",
+	"transparent-window-blur": "https://www.lunarvim.org/configuration/03-colorschemes.html#transparent-windows",
+
+	"core-nvim-tree-nvimtree-nvim_tree-plugins-dashboard-bufferline-terminal-toggleterm -dap-compe-cmp": "https://www.lunarvim.org/plugins/#core-plugins",
+	"install-plugins": "https://www.lunarvim.org/plugins/01-installing.html#example",
+	"remove-plugins-delete-plugins": "https://www.lunarvim.org/plugins/01-installing.html#removing-plugins",
+	"style-guide": "https://www.lunarvim.org/dev/#style-guide",
+	"treesitter-tree-sitter": "https://www.lunarvim.org/plugins/02-extra-plugins.html#treesitter",
+
+	"langs-cpp-javascript-php-python3-rust-ruby-luacsharp-go-json-scala-vue-svelte-react-typescript": "https://www.lunarvim.org/languages/",
+	"lvinfo-lunarvim-info": "https://www.lunarvim.org/languages/#lunarvim-info",
+	"install-lsp-lspinstall": "https://www.lunarvim.org/languages/#lsp",
+	"formatter-formatting": "https://www.lunarvim.org/languages/#formatting",
+	"linting": "https://www.lunarvim.org/languages/#linting",
+
+	"nice-noice-bruh-sus": "[LMAO loser](https://www.youtube.com/watch?v=dQw4w9WgXcQ)",
+
+	// PLUGINS
+	"vim-fugitive": "https://www.lunarvim.org/plugins/02-extra-plugins.html#vim-fugitive",
+	"trouble-folke": "https://www.lunarvim.org/plugins/02-extra-plugins.html#trouble-nvim",
+	"indent-blanklines": "https://www.lunarvim.org/plugins/02-extra-plugins.html#indent-blankline",
+	"compe-cmp-tabnine": "https://www.lunarvim.org/plugins/02-extra-plugins.html#compe-tabnine",
+	"telescope": "https://www.lunarvim.org/plugins/02-extra-plugins.html#telescope-fzy-native-nvim",
+	"minimap-plugin-minimap": "https://www.lunarvim.org/plugins/02-extra-plugins.html#minimap",
+	"ranger-plugin-ranger": "https://www.lunarvim.org/plugins/02-extra-plugins.html#rnvimr",
+	"git-diffview": "https://www.lunarvim.org/plugins/02-extra-plugins.html#diffview",
+	"octo-nvim-git": "https://www.lunarvim.org/plugins/02-extra-plugins.html#octo",
+	"nvim-ts-rainbow": "https://www.lunarvim.org/plugins/02-extra-plugins.html#nvim-ts-rainbow",
+	"autosave": "https://www.lunarvim.org/plugins/02-extra-plugins.html#autosave",
+	"markdown-preview-markdownpreview": "https://www.lunarvim.org/plugins/02-extra-plugins.html#markdown-preview-nvim",
+	"neoscroll": "https://www.lunarvim.org/plugins/02-extra-plugins.html#neoscroll",
+	"nvim-ts-autotag": "https://www.lunarvim.org/plugins/02-extra-plugins.html#nvim-ts-autotag",
+	"commentary-kommentary": "https://www.lunarvim.org/plugins/02-extra-plugins.html#nvim-ts-context-commentstring",
+	"todo-comments-folke": "https://www.lunarvim.org/plugins/02-extra-plugins.html#todo-comments-nvim",
+	"folke-lsp-colors": "https://www.lunarvim.org/plugins/02-extra-plugins.html#lsp-colors",
+	"persistence-folke": "https://www.lunarvim.org/plugins/02-extra-plugins.html#persistence",
+	"bracey": "https://www.lunarvim.org/plugins/02-extra-plugins.html#bracey"
+};
+
 /**
 *
 * @param {Client} client
@@ -28,71 +86,12 @@ module.exports = {
 	execute: async (client, message, args) => {
 		const search_query = args.join("-");
 		if (!args.length) return
-		// const args2 = args[1];
-
-		// TODO: need to add categories: languages, extra-plugins
-		const lunarDocs = {
-			"lunarvim-github": "https://github.com/LunarVim/LunarVim",
-			"installing-uninstalling": "https://www.lunarvim.org/01-installing.html#uninstall",
-			"stable": "https://www.lunarvim.org/01-installing.html#stable",
-			"rolling": "https://www.lunarvim.org/01-installing.html#rolling",
-
-			"configuration": "https://www.lunarvim.org/configuration/",
-			"nerd-fonts": "https://www.lunarvim.org/configuration/04-nerd-fonts.html",
-			"which-key-whichkey": "https://www.lunarvim.org/configuration/02-keybindings.html#whichkey-bindings",
-
-			"quickstart-after-install": "https://www.lunarvim.org/02-after-install.html",
-			"general-settings-options": "https://www.lunarvim.org/configuration/01-settings.html",
-			"keybindings-keybinds-keymaps-keymappings": "https://www.lunarvim.org/configuration/02-keybindings.html",
-			"autocmds-autocommands": "https://www.lunarvim.org/configuration/05-autocommands.html",
-			"color-schemes-colorschemes": "https://www.lunarvim.org/configuration/03-colorschemes.html",
-			"statusline-lualine-galaxyline": "https://www.lunarvim.org/configuration/06-statusline.html",
-
-			"general-keybinds-general-keybindings": "https://www.lunarvim.org/configuration/02-keybindings.html#general-bindings",
-			"leader-key": "https://www.lunarvim.org/configuration/02-keybindings.html#leader-key",
-			"transparent-window-blur": "https://www.lunarvim.org/configuration/03-colorschemes.html#transparent-windows",
-
-			"core-nvim-tree-nvim_tree-plugins-dashboard-bufferline-terminal-toggleterm-dap": "https://www.lunarvim.org/plugins/#core-plugins",
-			"install-plugins": "https://www.lunarvim.org/plugins/01-installing.html#example",
-			"remove-plugins-delete-plugins": "https://www.lunarvim.org/plugins/01-installing.html#removing-plugins",
-			"style-guide": "https://www.lunarvim.org/dev/#style-guide",
-			"treesitter": "https://www.lunarvim.org/plugins/02-extra-plugins.html#treesitter",
-
-			"langs-cpp-javascript-php-python-rust-ruby-luacsharp-go-json-scala-vue-svelte-react-typescript": "https://www.lunarvim.org/languages/",
-			"lvinfo-lunarvim-info": "https://www.lunarvim.org/languages/#lunarvim-info",
-			"install-lsp-lspinstall": "https://www.lunarvim.org/languages/#lsp",
-			"formatter-formatting": "https://www.lunarvim.org/languages/#formatting",
-			"linting": "https://www.lunarvim.org/languages/#linting",
-
-			// PLUGINS
-			"vim-fugitive": "https://www.lunarvim.org/plugins/02-extra-plugins.html#vim-fugitive",
-			"trouble-folke": "https://www.lunarvim.org/plugins/02-extra-plugins.html#trouble-nvim",
-			"indent-blanklines": "https://www.lunarvim.org/plugins/02-extra-plugins.html#indent-blankline",
-			"compe-cmp-tabnine": "https://www.lunarvim.org/plugins/02-extra-plugins.html#compe-tabnine",
-			"telescope": "https://www.lunarvim.org/plugins/02-extra-plugins.html#telescope-fzy-native-nvim",
-			"minimap-plugin-minimap": "https://www.lunarvim.org/plugins/02-extra-plugins.html#minimap",
-			"ranger-plugin-ranger": "https://www.lunarvim.org/plugins/02-extra-plugins.html#rnvimr",
-			"git-diffview": "https://www.lunarvim.org/plugins/02-extra-plugins.html#diffview",
-			"octo-nvim-git": "https://www.lunarvim.org/plugins/02-extra-plugins.html#octo",
-			"nvim-ts-rainbow": "https://www.lunarvim.org/plugins/02-extra-plugins.html#nvim-ts-rainbow",
-			"autosave": "https://www.lunarvim.org/plugins/02-extra-plugins.html#autosave",
-			"markdown-preview-markdownpreview": "https://www.lunarvim.org/plugins/02-extra-plugins.html#markdown-preview-nvim",
-			"neoscroll": "https://www.lunarvim.org/plugins/02-extra-plugins.html#neoscroll",
-			"nvim-ts-autotag": "https://www.lunarvim.org/plugins/02-extra-plugins.html#nvim-ts-autotag",
-			"commentary-kommentary": "https://www.lunarvim.org/plugins/02-extra-plugins.html#nvim-ts-context-commentstring",
-			"todo-comments-folke": "https://www.lunarvim.org/plugins/02-extra-plugins.html#todo-comments-nvim",
-			"folke-lsp-colors": "https://www.lunarvim.org/plugins/02-extra-plugins.html#lsp-colors",
-			"persistence-folke": "https://www.lunarvim.org/plugins/02-extra-plugins.html#persistence",
-			"bracey": "https://www.lunarvim.org/plugins/02-extra-plugins.html#bracey"
-
-		};
 
 		let count = 0;
 		Object.keys(lunarDocs).map((query) => {
 			if (query.includes(search_query)) {
 				count++;
 				const em = new MessageEmbed()
-					// .setTitle("LunarVim docs search results")
 					.setDescription(lunarDocs[query])
 					.setColor("RED");
 
